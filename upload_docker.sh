19f2e2d4794b98ae2bb3e2d47019cdcb5bcccee4
dinatahoun92 <dinatahoun992@gmail.com>
2020-07-04 00:53:55 +0000

    kubernetes

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath= dinatahoun/ml
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag ml $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
